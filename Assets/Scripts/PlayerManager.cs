c78bd54ade3abe666b6635ca4d99156c47e60518
NameOfFool <ivan-pauk2002@mail.ru>
2023-09-03 22:52:15 +0300

    init

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,8 +7,6 @@ public class PlayerManager : MonoBehaviourPunCallbacks
 {
     public int coinCounter = 0;
     public GameObject playerUIPrefab;
-
-    public GameObject CoinPrefab;
     private PhotonView view;
     private GameObject _pui;
     private void Start()
@@ -16,23 +14,10 @@ public class PlayerManager : MonoBehaviourPunCallbacks
         view = GetComponent<PhotonView>();
         if (view.IsMine)
         {
-            _pui = Instantiate(playerUIPrefab);
+            _pui = GameObject.Find(playerUIPrefab.name);
             _pui.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
         }
-        if (PhotonNetwork.IsMasterClient)
-        {
-            view.RPC("SpawnCoins",RpcTarget.MasterClient);
-        }
 
-    }
-
-    [PunRPC]
-    public void SpawnCoins()
-    {
-        for (int i = -7; i < 9; i++)
-        {
-            PhotonNetwork.Instantiate(CoinPrefab.name, new Vector3(i - 0.5f, 3.5f, 0f), Quaternion.identity);
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
